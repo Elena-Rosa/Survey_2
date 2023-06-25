99a0b3fad4ecb995c34eef3303ba60476eb64755
Elena <elenarosamail@gmail.com>
2023-06-25 02:07:19 -0700

    retrive users answers

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,9 @@ window.onload = function () {
 function submitForm() {
     event.preventDefault();
     const language = document.getElementById('language');
+
+    const answer1 = document.getElementById('fruitsQ')
+    const answer2 = document.getElementById('hotDrinkQ')
+    const answer3 = document.getElementById('dancingQ')
+    const answer4 = document.getElementById('morningPersonQ')
+    const answer5 = document.getElementById('devStyleQ')
