c45554e50866d252894cf563d75c8a61682c32ba
Elena <elenarosamail@gmail.com>
2023-06-25 07:59:45 -0700

    add if and if else statements

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,22 @@
 window.onload = function () {
     const surveyForm = document.getElementById("user info");
-    surveyForm.addEventListener("submit", submitForm)
+    surveyForm.addEventListener("submit", submitForm);
 }
-
+console.log(surveyForm)
 function submitForm() {
     event.preventDefault();
     const language = document.getElementById('language');
     language.classList.remove("hidden")
 
-    const answer1 = document.getElementById('fruitsQ')
-    const answer2 = document.getElementById('hotDrinkQ')
-    const answer3 = document.getElementById('dancingQ')
-    const answer4 = document.getElementById('morningPersonQ')
-    const answer5 = document.getElementById('devStyleQ')
+    const answer1 = document.getElementById('fruitsQ');
+    const answer2 = document.getElementById('hotDrinkQ');
+    const answer3 = document.getElementById('dancingQ');
+    const answer4 = document.getElementById('morningPersonQ');
+    const answer5 = document.getElementById('devStyleQ');
 
-    if (answer1 == "apples" & answer2 == "tea" && answer3 == "y" && answer4 = "y" && answer5 == "frontend")
+    if (answer1.value == "apples" && answer2.value == "tea" && answer2.value == "y" && answer3.value == "y" && answer4.value == "frontend") {
+        language = "Possibly JavaScript!"
+    }
+    else if (answer1.value == "oranges" && answer2.value == "coffee" && answer3.value == "no" && answer4.value == "no" && answer5.value == "backend") {
+        language = "We recommend C#!"
+    }
