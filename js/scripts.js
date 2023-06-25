eb27fe87915d3d69b1574d5ee5cda9353406b5e3
Elena <elenarosamail@gmail.com>
2023-06-25 06:56:33 -0700

    WIP, working on gathering form data

--- js/scripts.js
+++ js/scripts.js
@@ -6,9 +6,12 @@ window.onload = function () {
 function submitForm() {
     event.preventDefault();
     const language = document.getElementById('language');
+    language.classList.remove("hidden")
 
     const answer1 = document.getElementById('fruitsQ')
     const answer2 = document.getElementById('hotDrinkQ')
     const answer3 = document.getElementById('dancingQ')
     const answer4 = document.getElementById('morningPersonQ')
     const answer5 = document.getElementById('devStyleQ')
+
+    if (answer1 == "apples" & answer2 == "tea" && answer3 == "y" && answer4 = "y" && answer5 == "frontend")
